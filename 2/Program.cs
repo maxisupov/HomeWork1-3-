f8175dcb92051418a54b4ee73451d138112691b1
Maxim <max_isupov@mail.ru>
2022-09-16 19:05:19 +0300

    Решил задание на вывод большего и меньшего числа

---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Введите первое чило: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе чило: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"{numberA} больше числа {numberB}");
+}
+else
+{
+    Console.WriteLine($"{numberB} больше числа {numberA}");
+}
+if (numberA == numberB)
+{
+    Console.WriteLine($"{numberA} равен {numberB}");
+}
